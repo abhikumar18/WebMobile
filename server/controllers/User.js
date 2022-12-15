c6275fc3ed90b7e6ac111a674191c6b11862db6a
abhikumar18 <abhishek.it.msit@gmail.com>
2022-12-15 19:50:26 +0530

    Otp and Email Part 2

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,11 +1,12 @@
 import { User } from "../models/users.js";
 import { sendMail } from "../utils/sendMail.js";
+import { sendToken } from "../utils/sendToken.js";
 
 export const register = async(req,res) => {
     try {
 
         const {name,email,password} = req.body;
-        const {avatar} = req.files;
+        // const {avatar} = req.files;
 
         let user = await User.findOne({email});
 
@@ -21,11 +22,16 @@ export const register = async(req,res) => {
             name,
             email,
             password,
-            avatar,
+            avatar:{
+                public_id: "",
+                url:"",
+            },
             otp,
             otp_expiry:new Date(Date.now() + process.env.OTP_EXPIRE*60*1000)});
 
             await sendMail(email,"Verify your account", `Your OTP is ${otp}`);
+
+            sendToken(res,user,200,"OTP sent to your email,Please verify your account");
         
     } catch (error) {
         res.status(500).json({success:false, message: error.message });
--- server/models/users.js
+++ server/models/users.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -32,8 +33,20 @@ const userSchema = new mongoose.Schema({
               createdAt:Date,
             },
           ],
+
+    verified:{
+        type:Boolean,
+        default:false,
+    },
+
     otp:Number,
     otp_expiry:Date,
 });
+
+userSchema.methods.getJWTToken = function(){
+   return jwt.sign({ _id:this._id },process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_COOKIE_EXPIRE,
+    });
+};
 
 export const User = mongoose.model("User",userSchema)
